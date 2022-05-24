112cb183adc842860c431b96b788991d69c30ba2
Lane Shetron <laneshetron@gmail.com>
2022-05-23 21:06:19 -0400

    Parameterize device-index

--- runonce.sh
+++ runonce.sh
@@ -21,7 +21,7 @@ for i in {1..2}; do
         aws ec2 attach-network-interface \
           --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
           --instance-id "$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)" \
-          --device-index 1 \
+          --device-index $i \
           --network-interface-id $eni
         return_code=$?
     done
