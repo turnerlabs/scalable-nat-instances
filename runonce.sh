c5bd7f26d9ac79f288a1ff3b91fcc8f3cc5ff5c3
Lane Shetron <laneshetron@gmail.com>
2022-06-01 21:54:25 -0400

    runonce.sh cleanup make snat.sh scalable

--- runonce.sh
+++ runonce.sh
@@ -3,24 +3,27 @@
 return_code=1
 tags=${tags}
 
-aws ec2 modify-instance-attribute --no-source-dest-check \
-  --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
-  --instance-id "$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)"
+region=$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')
+instance_id=$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)
 
-# attach 2 interfaces for maximal bandwidth
+aws ec2 modify-instance-attribute --no-source-dest-check \
+  --region "$region" \
+  --instance-id "$instance_id"
+
+# attach one interface for each route
 for i in {1..${routes_count}}; do
     # retry attachment errors in case of contention
-    while [ $return_code -ne 0 ]; do
+    while (( return_code )); do
         # get the first (random) available interface
         eni=$(aws ec2 describe-network-interfaces \
-          --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
-          --filters "Name=group-id,Values=${sg_id}" "Name=tag:route,Values=$tags[$i]" "Name=status,Values=available" \
+          --region "$region" \
+          --filters "Name=group-id,Values=${sg_id}" "Name=tag:route,Values=${tags[i]}" "Name=status,Values=available" \
           --query 'NetworkInterfaces[0].NetworkInterfaceId' | tr -d '"')
 
         # attach the ENI
         aws ec2 attach-network-interface \
-          --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
-          --instance-id "$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)" \
+          --region "$region" \
+          --instance-id "$instance_id" \
           --device-index $i \
           --network-interface-id $eni
         return_code=$?
--- snat.sh
+++ snat.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -x
 
-# wait for eth1
-while ! ip link show dev eth1; do
-  sleep 1
+for i in {0..${routes_count}}; do
+    # wait for interface
+    while ! ip link show dev eth$i; do
+      sleep 1
+    done
+
+    sysctl -q -w net.ipv4.conf.eth$i.rp_filter=0
+
+    # enable IP forwarding and NAT
+    sysctl -q -w net.ipv4.conf.eth$i.send_redirects=0
 done
 
 sysctl -q -w net.ipv4.conf.all.rp_filter=0
-sysctl -q -w net.ipv4.conf.eth0.rp_filter=0
-sysctl -q -w net.ipv4.conf.eth1.rp_filter=0
 sysctl -q -w net.ipv4.conf.default.rp_filter=0
-
-
-# enable IP forwarding and NAT
 sysctl -q -w net.ipv4.ip_forward=1
-sysctl -q -w net.ipv4.conf.eth1.send_redirects=0
+
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # wait for network connection
